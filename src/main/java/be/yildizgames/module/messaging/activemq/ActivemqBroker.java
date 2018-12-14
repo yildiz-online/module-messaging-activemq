8473e5e356e70677cf80ab75af345bd0a8d9b4e5
Grégory Van den Borre <vandenborre.gregory@hotmail.fr>
2018-12-14 16:37:50 +0100

    [YE-0] Use logging 2.

--- src/main/java/be/yildizgames/module/messaging/activemq/ActivemqBroker.java
+++ src/main/java/be/yildizgames/module/messaging/activemq/ActivemqBroker.java
@@ -26,7 +26,6 @@
 package be.yildizgames.module.messaging.activemq;
 
 
-import be.yildizgames.common.exception.initialization.InitializationException;
 import be.yildizgames.module.messaging.Broker;
 import be.yildizgames.module.messaging.BrokerProperties;
 import be.yildizgames.module.messaging.exception.MessagingException;
